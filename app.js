1c48bba79a86b1f3579ea2e0d150d3a13e8728a5
Gab Santos <63992851+GabSantos0929@users.noreply.github.com>
2025-02-01 15:50:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
+const fs = require('fs');
 const app = express();
 const routes = require('./src/router');
 const initData = require('./src/initData');
